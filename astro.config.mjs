e43a16ea98f9b641860e2e93a71b0dd37fba2cb9
Siddharth Dhaigude <sd8capricon@gmail.com>
2024-08-28 16:45:32 +0530

    format astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
+// astro
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-
-import sanity from "@sanity/astro";
 import react from "@astrojs/react";
+
+// sanity
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
   // site: "https://eyeqsceurity.ca"
-  integrations: [sitemap(),
-  sanity({
-    projectId: "06moeryx",
-    dataset: "production",
-    useCdn: false,
-    studioBasePath: "/studio"
-  }),
-  react()]
+  integrations: [
+    sitemap(),
+    sanity({
+      projectId: "06moeryx",
+      dataset: "production",
+      useCdn: false,
+      studioBasePath: "/studio"
+    }),
+    react()
+  ]
 });
