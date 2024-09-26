01ec7bb8fcb87b70503c8a0f6eabd84eb45ac3df
Siddharth Dhaigude <sd8capricon@gmail.com>
2024-09-26 13:17:58 +0530

    services brief update

--- src/data/serviceBriefs.ts
+++ src/data/serviceBriefs.ts
@@ -10,20 +10,21 @@ const serviceBriefs: ServiceBreif[] = [
         image: TestImage,
         service: "General Security Services",
         link: "general-security",
-        description: "EyeQ Security Services provides comprehensive security services that protect and preserve businesses and properties throughout Manitoba.We take pride in saying our quality security solutions never fail to deliver.Our efficacious security programmes and dedicated team will maximise your coverage and give your site supreme protection."
+        description: "We offer unarmed uniform security guards services to Private Enterprises, Educational Institutions, Retail and shopping malls, Leisure and Amusement Parks, Industrial and Construction projects.Warehouses, Homes and Residential Properties, Labor Camps, etc.We follow highest standards of integrity and excellence at every level.To us, integrity and professionalism are of utmost importance.As an organization committed to excellence, we maintain the highest industry standards for security officer's training, honor, loyalty, and character.All of our security officers will be highly trained and are subject to constant site supervision and random inspections to effectively meet the demands of the market."
     },
     {
         id: "bank",
         link: "bank-security",
         image: TestImage,
-        service: "Bank Security Services"
+        service: "Bank Security Services",
+        description: "Banks are probably the riskiest and most sensitive businesses, and they need 24/7 comprehensive security and surveillance.EyeQ personnel assigned to banks are adequately trained for the banking environment and will ensure that all individuals entering the bank are scanned and have a legitimate reason for being within the banking premises.We have trained and fully equipped our security personnel to match these standards to provide highly proactive security and deliver beyond our clients&#39; expectations for all types of banking institutions.Our security guards will be stationed at highly visible places within the banking premises, which lowers the likelihood of crime."
     },
     {
         id: "hospital-senior-living",
         link: "hospital-senior-living",
         image: TestImage,
         service: "Hospital & Senior Living Security Services",
-        description: "Hospitals exist to provide care to those who are ill or injured. This means that many people who reside in hospitals and seniors living centers are vulnerable to exploitation, theft of their belongings, or even violence. Hospitals also typically maintain large inventories of drugs, some of which may have significant street value."
+        description: "Hospitals exist to provide care to those who are ill or injured. This means that many people who reside in hospitals and Seniors living centers are vulnerable to exploitation, theft of their belongings, or even violence.Hospitals also typically maintain large inventories of drugs, some of which may have significant street value.Healthcare employees, likewise, are also vulnerable as targets of violence for both patients and family members alike.A hospital security officer is charged with maintaining the peace in a hospital & amp; seniors living centers and working to safeguard the people and items within its walls."
     },
     {
         id: "hotel",
@@ -37,7 +38,7 @@ const serviceBriefs: ServiceBreif[] = [
         image: TestImage,
         service: "Loss Prevenion Security",
         link: "loss-prevention",
-        description: ""
+        description: "Retail crime costs Canadian firms over $5 billion each year. Independent retailers are a particularly vulnerable target, with 87.5 percent reporting at least one theft every year.When finances are tight, Canadian businesses can protect their earnings by ensuring a substantial loss prevention strategy."
 
     },
     {
@@ -45,48 +46,49 @@ const serviceBriefs: ServiceBreif[] = [
         link: "mall-security",
         image: TestImage,
         service: "Shopping Mall & Retail Security Services",
-        description: ""
+        description: "EyeQ Security Services has been a trusted security partner for some of the largest companies in Canadian retail since our debut. We've learned from working with notable companies that no two retail stores or shopping malls are alike and that each requires a custom security solution suited to its specific needs."
     },
     {
         id: "school",
         link: "school-security",
         image: TestImage,
         service: "School Security Services",
-        description: "School Security officers play a critical role by inspecting and patrolling premises regularly, monitoring property entrances, authorizing entrance of people and vehicles, reporting any suspicious behavior and happenings, securing all exit doors and windows, monitoring surveillance cameras, responding to alarms and reacting promptly, assisting people in need, and submitting reports of daily surveillance activities. Trust us for the safety of your employees and property."
+        description: "Hospitals exist to provide care to those who are ill or injured. This means that many people who reside in hospitals and Seniors living centers are vulnerable to exploitation, theft of their belongings, or even violence.Hospitals also typically maintain large inventories of drugs, some of which may have significant street value.Healthcare employees, likewise, are also vulnerable as targets of violence for both patients and family members alike.A hospital security officer is charged with maintaining the peace in a hospital & amp; seniors living centers and working to safeguard the people and items within its walls."
     },
     {
         id: "pubs-clubs",
         link: "pubs-clubs",
         image: TestImage,
         service: "Pubs & Clubs Security Services",
+        description: "Pub and club security is essential for maintaining a safe and enjoyable environment for patrons and staff alike. At EyeQ Security Services, we understand the unique challenges faced by establishments in the nightlife industry, and our comprehensive security services are tailored to meet these needs."
     },
     {
         id: "event",
         image: TestImage,
         service: "Event Security Services",
         link: "event-security",
-        description: "Hotel security is responsible for reporting all suspicious activity to their supervisors or hotel management. Usually, hotel security should report current events in real-time through their radio and document events that have already been resolved."
+        description: "Security is an essential part of any event and can mean the difference between success and failure for your special occasion. Ensuring that you hire qualified, professional event security staff is crucial, and this is where we can help you."
     },
     {
         id: "executive",
         link: "executive-protection",
         image: TestImage,
         service: "Executive Proctection Security Services",
-        description: ""
+        description: "Public figures, high-net-worth individuals, and VIPs may be exposed to heightened personal risk due to their status, careers, affiliations, geography, or net worth. With strategic risk assessment and advanced planning, experienced security teams protect these high-risk individuals from physical, financial, and reputational harm."
     },
     {
         id: "parking",
         link: "parking-enforcement",
         image: TestImage,
         service: "Parking Enforcement Services",
-        description: ""
+        description: "Maintaining a parking lot can be stressful, as patrons pay you to protect their vehicles. Since cars don't come cheap, it's of the utmost importance that you're able to keep them safe from harm while in your parking garage or parking lot. At EyeQ Security, we recognise that parking lot safety isn't always easy."
     },
     {
         id: "mobile-patrol",
         link: "mobile-patrol",
         image: TestImage,
         service: "Mobile Patrol",
-        description: "We understand at EyeQ Security Services that since parking lots are usually 24/7 operations, you might not want to invest in having security guards physically present all that time. Not only is that an expensive enterprise, but it's a lot to keep track of."
+        description: "We understand at EyeQ Security Services that since parking lots are usually 24/7 operations, you might not want to invest in having security guards physically present all that time.Not only is that an expensive enterprise, but it's a lot to keep track of."
     },
 
     {
@@ -94,13 +96,14 @@ const serviceBriefs: ServiceBreif[] = [
         link: "security-audit",
         image: TestImage,
         service: "Security Audit Services",
-        description: ""
+        description: "A security assessment identifies weak points in building security. EyeQ's security consulting team will provide Threat and Risk Assessments (TRAs) to a variety of clients across the Dubai area, enabling EyeQ's clients to take action and protect their properties and communities from liability."
     },
     {
         id: "threat-risk",
         link: "threat-risk-management",
         image: TestImage,
-        service: "Threat and Risk Mangement Services"
+        service: "Threat and Risk Mangement Services",
+        description: "While knowing how to respond to threats should be a priority, there's one thing that's more important and effective at protecting your property: prevention."
     }
 ]
 
