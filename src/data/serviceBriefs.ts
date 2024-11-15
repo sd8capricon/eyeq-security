df08ac77e0e6243bf5e38157c498ca87181a4829
Siddharth Dhaigude <sd8capricon@gmail.com>
2024-11-16 00:37:35 +0530

    service images

--- src/data/serviceBriefs.ts
+++ src/data/serviceBriefs.ts
@@ -1,13 +1,20 @@
 // Types
 import type { ServiceBreif } from "../types";
 // Assets
-import TestImage from "../assets/img/service/719-1280x720.jpg"
+import TestImage from "../assets/img/service/719-1280x720.jpg";
+import General from "../assets/img/service/general.jpg";
+import Bank from "../assets/img/service/bank.jpg";
+import Hospital from "../assets/img/service/hospital.jpg";
+import Retail from "../assets/img/service/retail.jpg";
+import Pubs from "../assets/img/service/pub.jpg";
+import Event from "../assets/img/service/event.jpg";
+import MobilePatrol from "../assets/img/service/mobile_patrol.jpg";
 
 const serviceBriefs: ServiceBreif[] = [
     {
         active: true,
         id: "general",
-        image: TestImage,
+        image: General,
         service: "General Security Services",
         link: "general-security",
         description: "We offer unarmed uniform security guards services to Private Enterprises, Educational Institutions, Retail and shopping malls, Leisure and Amusement Parks, Industrial and Construction projects.Warehouses, Homes and Residential Properties, Labor Camps, etc.We follow highest standards of integrity and excellence at every level.To us, integrity and professionalism are of utmost importance.As an organization committed to excellence, we maintain the highest industry standards for security officer's training, honor, loyalty, and character.All of our security officers will be highly trained and are subject to constant site supervision and random inspections to effectively meet the demands of the market."
@@ -15,14 +22,14 @@ const serviceBriefs: ServiceBreif[] = [
     {
         id: "bank",
         link: "bank-security",
-        image: TestImage,
+        image: Bank,
         service: "Bank Security Services",
         description: "Banks are probably the riskiest and most sensitive businesses, and they need 24/7 comprehensive security and surveillance.EyeQ personnel assigned to banks are adequately trained for the banking environment and will ensure that all individuals entering the bank are scanned and have a legitimate reason for being within the banking premises.We have trained and fully equipped our security personnel to match these standards to provide highly proactive security and deliver beyond our clients&#39; expectations for all types of banking institutions.Our security guards will be stationed at highly visible places within the banking premises, which lowers the likelihood of crime."
     },
     {
         id: "hospital-senior-living",
         link: "hospital-senior-living",
-        image: TestImage,
+        image: Hospital,
         service: "Hospital & Senior Living Security Services",
         description: "Hospitals exist to provide care to those who are ill or injured. This means that many people who reside in hospitals and Seniors living centers are vulnerable to exploitation, theft of their belongings, or even violence.Hospitals also typically maintain large inventories of drugs, some of which may have significant street value.Healthcare employees, likewise, are also vulnerable as targets of violence for both patients and family members alike.A hospital security officer is charged with maintaining the peace in a hospital & amp; seniors living centers and working to safeguard the people and items within its walls."
     },
@@ -44,7 +51,7 @@ const serviceBriefs: ServiceBreif[] = [
     {
         id: "mall-retail",
         link: "mall-security",
-        image: TestImage,
+        image: Retail,
         service: "Shopping Mall & Retail Security Services",
         description: "EyeQ Security Services has been a trusted security partner for some of the largest companies in Canadian retail since our debut. We've learned from working with notable companies that no two retail stores or shopping malls are alike and that each requires a custom security solution suited to its specific needs."
     },
@@ -58,13 +65,13 @@ const serviceBriefs: ServiceBreif[] = [
     {
         id: "pubs-clubs",
         link: "pubs-clubs",
-        image: TestImage,
+        image: Pubs,
         service: "Pubs & Clubs Security Services",
         description: "Pub and club security is essential for maintaining a safe and enjoyable environment for patrons and staff alike. At EyeQ Security Services, we understand the unique challenges faced by establishments in the nightlife industry, and our comprehensive security services are tailored to meet these needs."
     },
     {
         id: "event",
-        image: TestImage,
+        image: Event,
         service: "Event Security Services",
         link: "event-security",
         description: "Security is an essential part of any event and can mean the difference between success and failure for your special occasion. Ensuring that you hire qualified, professional event security staff is crucial, and this is where we can help you."
@@ -86,7 +93,7 @@ const serviceBriefs: ServiceBreif[] = [
     {
         id: "mobile-patrol",
         link: "mobile-patrol",
-        image: TestImage,
+        image: MobilePatrol,
         service: "Mobile Patrol",
         description: "We understand at EyeQ Security Services that since parking lots are usually 24/7 operations, you might not want to invest in having security guards physically present all that time.Not only is that an expensive enterprise, but it's a lot to keep track of."
     },
